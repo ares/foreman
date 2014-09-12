e01ba50ee7ae3faae6f4d1f7980625258dada4d5
Greg Sutcliffe <gsutclif@redhat.com>
2014-09-12 11:32:14 +0100

    Fixes #7733 - Remove hosts.yml fixtures and use FactoryGirl instead

--- test/factories/host_related.rb
+++ test/factories/host_related.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :host do
     sequence(:name) { |n| "host#{n}" }
+    sequence(:hostname) { |n| "host#{n}" }
+    sequence(:ip) { |n| IPAddr.new(n, Socket::AF_INET).to_s }
+    sequence(:mac) { |n| "01:23:45:67:89:" + ("%02x" % "#{n%256}") }
+    root_pass 'xybxa6JUkz63w'
     domain
     environment
 
@@ -9,11 +13,15 @@ FactoryGirl.define do
     end
 
     trait :with_hostgroup do
-      hostgroup :environment => environment
+      hostgroup { FactoryGirl.create(:hostgroup, :environment => environment) }
     end
 
     trait :with_puppetclass do
       puppetclasses { [ FactoryGirl.create(:puppetclass, :environments => [environment]) ] }
+    end
+
+    trait :with_config_group do
+      config_groups { [ FactoryGirl.create(:config_group, :with_puppetclass, :class_environments => [environment]) ] }
     end
 
     trait :with_parameter do
@@ -53,6 +61,85 @@ FactoryGirl.define do
     trait :with_subnet do
       subnet
     end
+
+    trait :with_operatingsystem do
+      operatingsystem
+    end
+
+    trait :with_puppet do
+      puppet_proxy { FactoryGirl.create(:smart_proxy,
+                        :features => [FactoryGirl.create(:feature, :puppet)]) }
+    end
+
+    trait :managed do
+      managed true
+      architecture
+      medium
+      ptable
+      location
+      organization
+      operatingsystem { FactoryGirl.create(:operatingsystem,
+                          :architectures => [architecture],
+                          :ptables => [ptable],
+                          :media => [medium])
+      }
+    end
+
+    trait :with_dhcp_orchestration do
+      managed true
+      architecture
+      association :compute_resource, :factory => :libvirt_cr
+      ptable
+      operatingsystem { FactoryGirl.create(:operatingsystem,
+                          :architectures => [architecture], :ptables => [ptable])
+      }
+      subnet { 
+        FactoryGirl.create(
+          :subnet,
+          :dhcp => FactoryGirl.create(:smart_proxy,
+                     :features => [FactoryGirl.create(:feature, :dhcp)])
+        )
+      }
+      ip { subnet.network.sub(/0\Z/, '1') }
+    end
+
+    trait :with_dns_orchestration do
+      managed true
+      architecture
+      association :compute_resource, :factory => :libvirt_cr
+      ptable
+      operatingsystem { FactoryGirl.create(:operatingsystem,
+                          :architectures => [architecture], :ptables => [ptable])
+      }
+      subnet {
+        FactoryGirl.create(:subnet,
+          :dns => FactoryGirl.create(:smart_proxy,
+                    :features => [FactoryGirl.create(:feature, :dns)])
+        )
+      }
+      domain {
+        FactoryGirl.create(:domain,
+          :dns => FactoryGirl.create(:smart_proxy,
+                    :features => [FactoryGirl.create(:feature, :dns)])
+        )
+      }
+      ip { subnet.network.sub(/0\Z/, '1') }
+    end
+
+    trait :with_puppet_orchestration do
+      managed true
+      architecture
+      location
+      organization
+      association :compute_resource, :factory => :libvirt_cr
+      ptable
+      operatingsystem { FactoryGirl.create(:operatingsystem,
+                                           :architectures => [architecture], :ptables => [ptable] ) }
+      puppet_ca_proxy { FactoryGirl.create(:smart_proxy,
+                                            :features => [FactoryGirl.create(:feature, :puppetca)])
+      }
+    end
+
   end
 
   factory :hostgroup do
@@ -83,6 +170,12 @@ FactoryGirl.define do
     end
   end
 
+  factory :ptable do
+    sequence(:name) { |n| "ptable#{n}" }
+    layout 'zerombr yes\nclearpart --all    --initlabel\npart /boot --fstype ext3 --size=<%= 10 * 10 %> --asprimary\npart /     --f   stype ext3 --size=1024 --grow\npart swap  --recommended'
+  end
+
+
   factory :parameter do
     sequence(:name) { |n| "parameter#{n}" }
     sequence(:value) { |n| "parameter value #{n}" }
@@ -103,9 +196,11 @@ FactoryGirl.define do
   end
   factory :nic_managed, :class => Nic::Interface, :parent => :nic_base do
     type 'Nic::Managed'
+    sequence(:mac) { |n| "01:23:45:ab:cd:" + ("%02x" % "#{n%256}") }
   end
   factory :nic_bmc, :class => Nic::Interface, :parent => :nic_base do
     type 'Nic::BMC'
+    sequence(:mac) { |n| "01:23:45:ab:ef:" + ("%02x" % "#{n%256}") }
   end
   factory :nic_bond, :class => Nic::Bond, :parent => :nic_base do
     type 'Nic::Bond'
