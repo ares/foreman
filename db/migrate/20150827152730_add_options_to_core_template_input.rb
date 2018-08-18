2f0a9157b6b5651c4b9385eff913e7212ecc4de6
Marek Hulan <mhulan@redhat.com>
2018-08-18 04:54:28 +0200

    Make migrations compatible with rex ones

--- db/migrate/20150827152730_add_options_to_core_template_input.rb
+++ db/migrate/20150827152730_add_options_to_core_template_input.rb
@@ -1,4 +1,4 @@
-class AddOptionsToTemplateInput < ActiveRecord::Migration[4.2]
+class AddOptionsToCoreTemplateInput < ActiveRecord::Migration[4.2]
   def change
     add_column :template_inputs, :options, :text
   end
