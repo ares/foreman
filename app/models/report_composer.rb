92ffae31a6aec80ea08c4160bd6ae74fdbd45674
Marek Hulan <mhulan@redhat.com>
2018-10-02 14:09:07 +0200

    Add optional fix from last REX PR

--- app/models/report_composer.rb
+++ app/models/report_composer.rb
@@ -8,8 +8,19 @@ class ReportComposer
 
     validates :value, :presence => true, :if => proc { |v| v.template_input.required? || v.value.present? }
 
-    validates :value, :inclusion => { :in => proc { |v| v.template_input.options_array } },
+    validates :value, :inclusion => { :in => proc { |v| options_for_template_input v.template_input } },
               :if => proc { |v| v.template_input.input_type == 'user' && v.template_input.options_array.present? }
+
+    class << self
+
+      private
+
+      def options_for_template_input(template_input)
+        options = template_input.options_array
+        options += [''] unless template_input.required?
+        options
+      end
+    end
   end
 
   class UiParams
